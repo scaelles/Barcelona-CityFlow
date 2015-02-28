b24519133f78af1e5275c2a6397814d9815d4078
rboada <ricardboada@gmail.com>
2015-02-28 10:53:49 +0100

    Canvis Ricard
    Changes:
    - getInstaData.php: Ja parseja en JSON, no s'ha provat perquè no hi ha
    dades a la BD.
    - index.php: Tot preparat perquè @Bertu faci maleses.
    - He esborrat milions de coses que no toquen.

--- getInstaData.php
+++ getInstaData.php
@@ -5,62 +5,69 @@
 	
 	// //Structure JSON file
 	// [{
-        // "idcell": 1,
+        // "idDistrict": 1,
         // "name": "Gràcia",
-		// "lat": XXX,
-		// "long": YYY,
-		// "rad": ZZZ,
-		// "posts": [{
-			// "lat": lat,
-			// "long": long,
-			// "im_link": link,
-			// "tags": tags
-			// },{}]
-        // "numLastPosts": #number
-        // }
-    // },
-    // {}]
+		// "bounds": XXX,
+		// "barris": [{
+			//	"idBarri": 1,
+			//	"name": "Vallcarca",
+			//	"bounds": XXX,
+			//	"posts": [{
+				// "lat": lat,
+				// "long": long,
+				// "im_link": link,
+				// "tags": tags
+				// }],
+			// "numLastPosts": #number
+        // }],
+    // }]
 	$outputJSONarray = array();
 	$c=0;
-	$resCells = mysqli_query($con, "SELECT * FROM cells") or die("Error in cells query<br>".mysqli_error($con));
-	while($rowCells=mysqli_fetch_array($resCells)){
-		//For each cell, we have to look for the last 3 posts in the social network, and count posts in last period of time.
-		$idCell = $rowCells['idcells'];
-		$nomCell = $rowCells['name'];
-		$latCell = $rowCells['lat'];
-		$longCell = $rowCells['long'];
-		$radCell = $rowCells['rad'];
+	$arrayDistr=array();
+	$resDistr = mysqli_query($con, "SELECT * FROM districts") or die("Error in districts query<br>".mysqli_error($con));
+	while($rowDistr=mysqli_fetch_array($resDistr)){
+		//For each district, we get all the neighbourhoods
+		$idDistrict = $rowDistr['idDistrict'];
+		$nameDistrict = $rowDistr['name'];
+		$boundsDistrict = $rowDistr['bounds'];
 		
-		//Count posts in last X minutes
-		$resNumPosts=mysqli_query($con,"SELECT COUNT(idposts) FROM posts WHERE date BETWEEN DATE_SUB(NOW(),INTERVAL 120 MINUTE) AND NOW() AND idcells='$idCell'") or die(mysqli_error($con));
-		if(mysqli_num_rows($resNumPosts)>0){
-			$numPostsCell = mysqli_fetch_array($resNumPosts)[0];
-		}else{
-			$numPostsCell=0;
-		}
-		//echo $numPostsCell;
-		
-		//Retrieve 3 last images in neighbourhood
-		$cPosts=0;
-		$arrayPosts=array();
-		$resPosts=mysqli_query($con,"SELECT im_link, tags, lat, posts.long FROM posts WHERE idcells='$idCell' ORDER BY date DESC LIMIT 3") or die(mysqli_error($con));
-		while($rowPosts=mysqli_fetch_array($resPosts)){ 
-			$tagsPost=$rowPosts['tags'];
-			$linkPost=$rowPosts['im_link'];
-			$latPost=$rowPosts['lat'];
-			$longPost=$rowPosts['long'];
+		$cNeigh=0;
+		$resNeigh = mysqli_query($con, "SELECT * FROM neighbourhoods WHERE idDistrict='$idDistrict'") or die("Error in neighbourhoods query<br>".mysqli_error($con));
+		while($rowNeigh=mysqli_fetch_array($resNeigh)){
+			//we have to look for the last 3 posts in the social network, and count posts in last period of time.
+			$idNeigh = $rowNeigh['idNeighb'];
+			$nameNeigh = $rowNeigh['name'];
+			$boundsNeigh = $rowNeigh['bounds'];
+						
+			//Count posts in last X minutes
+			$resNumPosts=mysqli_query($con,"SELECT COUNT(idPost) FROM posts WHERE date BETWEEN DATE_SUB(NOW(),INTERVAL 120 MINUTE) AND NOW() AND idNeighb='$idNeigh'") or die(mysqli_error($con));
+			if(mysqli_num_rows($resNumPosts)>0){
+				$numPostsNeigh = mysqli_fetch_array($resNumPosts)[0];
+			}else{
+				$numPostsNeigh=0;
+			}
 			
-			$arrayPosts[$cPosts]=array('lat'=>$latPost, 'long'=>$longPost, 'link'=>$linkPost, 'tags'=>$tagsPost);
-			$cPosts++;
+			//Retrieve 3 last images in neighbourhood
+			$cPosts=0;
+			$arrayPosts=array();
+			$resPosts=mysqli_query($con,"SELECT im_link, tags, lat, posts.long FROM posts WHERE idNeighb='$idNeigh' ORDER BY date DESC LIMIT 3") or die(mysqli_error($con));
+			while($rowPosts=mysqli_fetch_array($resPosts)){ 
+				$tagsPost=$rowPosts['tags'];
+				$linkPost=$rowPosts['im_link'];
+				$latPost=$rowPosts['lat'];
+				$longPost=$rowPosts['long'];
+				
+				$arrayPosts[$cPosts]=array('lat'=>$latPost, 'long'=>$longPost, 'link'=>$linkPost, 'tags'=>$tagsPost);
+				$cPosts++;
+			}
+			//Write output array
+			$arrayNeigh[$cNeigh]=array('idNeigh'=>$idNeigh,'name'=>utf8_encode($nameNeigh), 'bounds'=>$boundsNeigh, 'numLastPosts'=>$numPostsNeigh, 'posts'=>$arrayPosts);
+			$cNeigh++;
 		}
-		
-		//Write output array
-		$arrayCell=array('idCell'=>$idCell,'nameCell'=>utf8_encode($nomCell), 'lat'=>$latCell, 'long'=>$longCell, 'rad'=>$radCell, 'numLastPosts'=>$numPostsCell, 'posts'=>$arrayPosts);
-		
-		$outputJSONarray[$c]=$arrayCell;
+		$arrayDistr[$c]=array('idDistrict'=>$idDistrict, 'nameDistrict'=>$nameDistrict,'boundsDistrict'=>boundsDistrict,'neighbs'=>$arrayNeigh);
 		$c++;
 	}
-	$plainJSON = json_encode($outputJSONarray);
+	$plainJSON = json_encode($arrayDistr);
 	echo $plainJSON;
 			
 ?>
