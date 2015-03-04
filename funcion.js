3b3f6cfc1faba109d9dadf2fcde12d5a98a70849
rboada <ricardboada@gmail.com>
2015-03-04 22:37:35 +0100

    Inicial

--- funcion.js
+++
@@ -1,8 +0,0 @@
-function cargarseccion(seccion){
-new Ajax.Request('seccion.php', {
-method: 'post',
-parameters: { seccion: seccion},
-onCreate: function(){ $('footer').innerHTML = 'Espera, por favor...'; },
-onSuccess: function(transport){ $('footer').innerHTML = transport.responseText; }
-});
-}
